3b07a300e6fc5e616ebf7c9994188dd26200f0e9
Levi Romandine <LRomandine@users.noreply.github.com>
2024-06-17 22:32:57 -0700

    Update 15-reboot-aps.sh
    Fixed reboot command and updated to use correct cron.

--- scripts/unifi_reboot_APs/15-reboot-aps.sh
+++ scripts/unifi_reboot_APs/15-reboot-aps.sh
@@ -1,34 +1,38 @@
 #!/bin/sh
 
-# Discover all APs
+DATA_DIR="/data"
+case "$(ubnt-device-info firmware || true)" in
+1*)
+    DATA_DIR="/mnt/data"
+    ;;
+2*)
+    DATA_DIR="/data"
+    ;;
+3*)
+    DATA_DIR="/data"
+    ;;
+*)
+    echo "ERROR: No persistent storage found." 1>&2
+    exit 1
+    ;;
+esac
+
+# Discover all WAPs
 ubnt-tools ubnt-discover > /tmp/UAP-list
 cat /tmp/UAP-list | grep UAP |awk '{print $2}' | sort> /tmp/UAP-list-IPs
 
-# Save our current crontab
-crontab -l > /tmp/rootcrontab
-
-# Loop through our discovered APs and create a crontab line for each in ascending order
+# Loop through our discovered WAPs and create a crontab line for each in ascending order
+echo 'MAILTO=""' > /tmp/rootcrontab
 COUNTER=$(expr 0)
 while read IP; do
     COUNTER=$(expr $COUNTER + 5)
-    echo "${COUNTER} 03 * * 1 ssh ${IP} 'shutdown -r now'" >> /tmp/rootcrontab
+    echo "${COUNTER} 03 * * 1 root ssh -o StrictHostKeychecking=no ${IP} 'reboot' && echo \"\`date +'%Y-%m-%d %H:%M:%S'\` - Reboot ${IP}\" >> /var/log/reboots.log 2>&1" >> /tmp/rootcrontab
 done < /tmp/UAP-list-IPs
 
-# Install the new crontab
-crontab /tmp/rootcrontab
+# Install the new crontab will be done via script 25-add-cron-jobs.sh
+#    https://github.com/unifi-utilities/unifios-utilities/blob/main/on-boot-script/examples/udm-files/on_boot.d/25-add-cron-jobs.sh
 
 # Cleanup
 rm /tmp/rootcrontab
 rm /tmp/UAP-list
 rm /tmp/UAP-list-IPs
-
-
-
-
-
-
-
-
-
-
-
